a8adce85d927d3eae3d94fe26870e57fdcd6ec66
lukaspkl <fohrenoficial@Live.com>
2024-03-22 00:52:21 -0300

    commit inicial

---
+++ src/main/java/com/microservice/compras/MicroserviceComprasApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceComprasApplication.class, args);
+	}
+
+}
